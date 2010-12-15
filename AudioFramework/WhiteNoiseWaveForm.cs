3797d03b4382b451096a7e8386c1aa44b5b00cf0
vanch3d <nicolas.github@calques3d.org>
2010-12-15 16:07:02 +0000

    Added support for White Noise Waveform (background noise)

---
+++ AudioFramework/WhiteNoiseWaveForm.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Kindohm.KSynth.Library
+{
+    public class WhiteNoiseWaveForm : IWaveForm
+    {
+        Random _rd = new Random();
+
+        public short GetSample(ushort phaseAngle)
+        {
+           // return phaseAngle < (ushort)short.MaxValue ? short.MinValue : short.MaxValue;
+
+            return (short)_rd.Next((int)short.MinValue, (int)short.MaxValue);
+        }
+
+    }
+}
