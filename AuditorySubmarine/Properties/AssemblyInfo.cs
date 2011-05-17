b17a95c36f60a1931cdb59b23f5bc22199c393ee
vanch3d <nicolas.github@calques3d.org>
2011-05-17 15:16:10 +0000

    - readying both games for beta release

--- AuditorySubmarine/Properties/AssemblyInfo.cs
+++ AuditorySubmarine/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("2.2.40.1603")]
-[assembly: AssemblyFileVersion("2.2.40.1603")]
+[assembly: AssemblyVersion("2.3.1.1615")]
+[assembly: AssemblyFileVersion("2.3.1.1615")]
